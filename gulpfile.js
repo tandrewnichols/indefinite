5afb3597c040a8a65e709a5e6928f93d48a7d7f3
Andrew Nichols <tandrewnichols@gmail.com>
2018-01-20 22:20:19 -0500

    Revert to only running phantom on travis

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ require('file-manifest').generate('./gulp', { match: '*.js' });
 
 gulp.task('build', gulp.series('clean:dist', 'spawn:webpack'));
 gulp.task('cover', gulp.series('clean:coverage', 'spawn:nyc'));
-gulp.task('ci', gulp.series(gulp.parallel('lint', 'cover', 'browser'), 'codeclimate'));
+gulp.task('ci', gulp.series(gulp.parallel('lint', 'cover', 'phantom'), 'codeclimate'));
 gulp.task('test', gulp.series('cover', 'browser', 'build'));
 gulp.task('unit', gulp.series('spawn:nyc'));
 gulp.task('default', gulp.series('lint', 'test'));
