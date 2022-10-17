ba556c992526a70487032b35bb93023e15a65b0b
Andrew Nichols <tandrewnichols@gmail.com>
2022-10-17 11:33:01 -0400

    Account for bizarre edge case with the word red

--- dist/indefinite.js
+++ dist/indefinite.js
@@ -126,6 +126,10 @@ exports.check = function (word, ending) {
     // If the word ends in the ending, remove it.
     var regex = new RegExp("".concat(ending, "$"));
     word = word.replace(regex, '');
+
+    if (word.length <= 1) {
+      return false;
+    }
   }
 
   return exports.list.indexOf(word) > -1;
@@ -160,8 +164,8 @@ exports.list = [// Nouns: eu like y
 'one', 'ouija', // Adjectives: o like w
 'once', // Adverbs: o like w
 // Nouns: u like y
-'ubiquity', 'udometer', 'ufo', 'uke', 'ukelele', 'ululate', 'unicorn', 'unicycle', 'uniform', 'unify', 'union', 'unison', 'unit', 'unity', 'universe', 'university', 'upas', 'ural', 'uranium', 'urea', 'ureter', 'urethra', 'urine', 'urologist', 'urology', 'urus', 'usage', 'use', 'user', 'usual', 'usurp', 'usury', 'utensil', 'uterus', 'utility', 'utopia', 'utricle', 'uvarovite', 'uvea', 'uvula', 'utah', 'utahn', // Adjectives: u like y
-'ubiquitous', 'ugandan', 'ukrainian', 'unanimous', 'unicameral', 'unified', 'unique', 'unisex', 'universal', 'urinal', 'urological', 'useful', 'useless', 'usurious', 'usurped', 'utilitarian', 'utopic', // Adverbs: u like y
+'ubiquity', 'udometer', 'ufo', 'uke', 'ukelele', 'ululate', 'unicorn', 'unicycle', 'uniform', 'unify', 'union', 'unison', 'unit', 'unity', 'universe', 'university', 'upas', 'ural', 'uranium', 'urea', 'ureter', 'urethra', 'urine', 'urologist', 'urology', 'urus', 'usage', 'use', 'user', 'usual', 'usurp', 'usurper', 'usury', 'utensil', 'uterus', 'utility', 'utopia', 'utricle', 'uvarovite', 'uvea', 'uvula', 'utah', 'utahn', // Adjectives: u like y
+'ubiquitous', 'ugandan', 'ukrainian', 'unanimous', 'unicameral', 'unified', 'unique', 'unisex', 'universal', 'urinal', 'urological', 'useful', 'useless', 'usurious', 'utilitarian', 'utopic', // Adverbs: u like y
 'ubiquitously', 'unanimously', 'unicamerally', 'uniquely', 'universally', 'urologically', 'usefully', 'uselessly', 'usuriously', // Nouns: y like i
 'yttria', 'yggdrasil', 'ylem', 'yperite', 'ytterbia', 'ytterbium', 'yttrium', // Adjectives: y like i
 'ytterbous', 'ytterbic', 'yttric', // Single letters
